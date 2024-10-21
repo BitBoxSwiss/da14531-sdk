1dee44d670c2dbaba262527610a9c3d110af7bff
Niklas Dusenlund <niklas.dusenlund@bitbox.swiss>
2024-10-21 11:32:27 +0200

    gapc: Add message type SET_LE_PKT_SIZE_CMD

--- src/ble_stack/host/gap/gapc.rs
+++ src/ble_stack/host/gap/gapc.rs
@@ -2,13 +2,17 @@ pub mod task {
     pub use crate::bindings::{
         gapc_connection_req_ind as GapcConnectionReqInd, gapc_disconnect_ind as GapcDisconnectInd,
         gapc_get_info_cmd as GapcGetInfoCmd, gapc_msg_id as GapcMsgId,
-        gapc_msg_id::GAPC_GET_INFO_CMD, gapc_msg_id::GAPC_PARAM_UPDATED_IND,
-        gapc_msg_id::GAPC_PARAM_UPDATE_CMD,
+        gapc_msg_id::{
+            GAPC_GET_INFO_CMD, GAPC_PARAM_UPDATED_IND, GAPC_PARAM_UPDATE_CMD,
+            GAPC_SET_LE_PKT_SIZE_CMD,
+        },
         gapc_operation_GAPC_GET_PEER_FEATURES as GAPC_GET_PEER_FEATURES,
         gapc_param_update_cmd as GapcParamUpdateCmd, gapc_param_updated_ind as GapcParamUpdatedInd,
+        gapc_set_le_pkt_size_cmd as GapcSetLePktSizeCmd,
     };
     use crate::platform::core_modules::ke::msg::kernel_msg_type;
 
     kernel_msg_type!(GapcParamUpdateCmd, GAPC_PARAM_UPDATE_CMD);
     kernel_msg_type!(GapcGetInfoCmd, GAPC_GET_INFO_CMD);
+    kernel_msg_type!(GapcSetLePktSizeCmd, GAPC_SET_LE_PKT_SIZE_CMD);
 }
