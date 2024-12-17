4c2a6a6e0b5936e1a1b1d8d7fa00dd37c569d3bd
Niklas Dusenlund <niklas.dusenlund@bitbox.swiss>
2024-12-17 09:36:40 +0100

    format

--- bindings.h
+++ bindings.h
@@ -2,36 +2,37 @@
 
 // app_modules
 #include "app.h"
+#include "app_callback.h"
+#include "app_customs.h"
 #include "app_easy_gap.h"
 #include "app_entry_point.h"
 #include "app_prf_types.h"
-#include "app_customs.h"
 #include "app_task.h"
-#include "app_callback.h"
 
 // ble_stack
-#include "custs1_task.h"
+#include "attm_db_128.h"
 #include "custs1.h"
+#include "custs1_task.h"
 #include "gap.h"
 #include "gapc_task.h"
 #include "gapm_task.h"
-#include "attm_db_128.h"
-#include "llm.h"
 #include "l2cm.h"
+#include "llm.h"
 
 // platform
-#include "syscntl.h"
-#include "system_library.h"
-#include "ke_msg.h"
-#include "rwip_config.h"
-#include "rwip.h"
 #include "aes.h"
+#include "ke_msg.h"
 #include "otp_cs.h"
 #include "rf_531.h"
+#include "rwip.h"
+#include "rwip_config.h"
 #include "spi.h"
 #include "spi_flash.h"
+#include "syscntl.h"
+#include "system_library.h"
 
-const uint8_t APP_MODULES_TIMER_MAX_NUM = (APP_TIMER_API_LAST_MES - APP_TIMER_API_MES0 + 1);
+const uint8_t APP_MODULES_TIMER_MAX_NUM =
+    (APP_TIMER_API_LAST_MES - APP_TIMER_API_MES0 + 1);
 const uint16_t APP_MODULES_TIMER_API_MES0 = APP_TIMER_API_MES0;
 const uint16_t APP_MODULES_TIMER_API_LAST_MES = APP_TIMER_API_LAST_MES;
 const uint32_t MSG_APP_CREATE_TIMER = APP_CREATE_TIMER;
